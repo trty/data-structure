5150e283680b41b6195f11c60c91952bcd42111e
Cookie <thecookie@qq.com>
2019-04-30 23:43:16 +0800

    初始化

---
+++ 1.cpp
@@ -0,0 +1,92 @@
+﻿#include "iostream"  //导入头文件
+using namespace std;  //标准输出
+const int maxsize = 100; //定义最大数据
+typedef char DataType; //定义Datetype为char类型
+class List { //定义List雷
+	DataType data[maxsize]; //初始化data数组 
+	int size; //初始化size
+public: //以下是公共函数,先定义,后面实现
+	List() { size = 0; } //构造函数,这里定义了size初始值
+	void insert(DataType d,int n); 
+	DataType Delete(int n);
+	DataType GetElem(int n);
+	bool ifempty();
+	int Locate(DataType d);
+	int Length();
+	void clear();
+	void print();
+};
+void List::insert(DataType d,int n) { //实现了插入函数
+	if (n<1 || n>size+1) {  //判断插入函数的位置参数是否合法
+		cout << "插入位置错误!" << endl;
+	}
+	else { //合法则继续
+		for (int i = size-1; i > n-2; i--) { //循环后移
+			data[i] = data[i - 1];
+		}
+		data[n - 1] = d; //插入值
+		size++; //列表长度加一
+	}
+}
+DataType List::Delete(int n) { //实现删除函数
+	if (n<1 || n>size ||size==0) { //判断参数是否合法
+		cout << "删除位置错误!" << endl;
+		exit(0); //退出
+	}
+	else {
+		DataType d = data[n]; //备份删除的值
+		for (int i = n - 1; n < size - 1; i++) { //循环前移
+			data[i] = data[i + 1];
+		}
+		size--; //列表长度减一
+		return d; //返回删除的值
+	}
+};
+DataType List::GetElem(int n) { //实现获取元素函数
+	if (n<1 || n>size||size==0) { //判断参数是否合法
+		cout << "获取位置错误!" << endl; //报错
+		exit(0); //退出
+	}
+	else {
+		return data[n-1]; //返回取值
+	}
+};
+bool List::ifempty() { //实现是否为空的判断
+	return size == 0 ? true : false; //使用三目运算符返回结果
+};
+int List::Locate(DataType d) { //实现定位函数
+	for (int i = 0; i < size; i++) {
+		if (data[i] == d) {
+			return i;
+		}
+	}
+	return -1; //无相同值返回-1
+};
+int List::Length() { //实现获取长度函数
+	return size;
+};
+void List::clear() { //实现清除函数
+	size = 0;
+};
+void List::print() { //实现输出函数
+	if (size == 0) { //长度为0则提示
+		cout << "列表为空!" << endl;
+	}else {
+		for (int i = 0; i < size; i++) { //循环输出
+			cout << data[i];
+		}
+		cout << endl;
+	}
+}
+int main() { //主函数
+	List l; 
+	l.insert('1',1);
+	l.insert('3', 2);
+	l.print();
+	cout << "获取第二个元素:"<<l.GetElem(2) << endl;
+	l.Delete(1);
+	l.print();
+	cout << "判断是否为空:"<<l.ifempty() << endl;
+	l.clear();
+	cout << "判断是否为空:"<<l.ifempty() << endl;
+}
---
+++ 3.cpp
@@ -0,0 +1,100 @@
+#include "iostream"//声明头文件
+using namespace std;//标准化输出
+const int stackmax=3;//定义顺序表栈的数据长度
+typedef char DataType;//定义两张栈的储存数据类型
+class Stack_1{//顺序表栈的类
+	DataType data[stackmax];//储存数据数组
+	int top;//类似于指针的顶
+public://定义类的公共方法
+	Stack_1(){//构造方法
+		top=-1;//初始化顶
+	};
+	bool ifEmpty(){//判断栈是否为空的方法
+		return top==-1?1:0;
+	};
+	bool isFull(){//判断是否栈满的方法
+		return top==stackmax?1:0;
+	};
+	void push(DataType d){//插入栈顶
+		if(top<stackmax-1){
+			data[++top] = d;
+		}else{
+			cout<<"栈已满!"<<endl;
+		}
+	};
+	DataType pop(){//取出栈顶
+		if(top==-1){
+			cout<<"栈已空!"<<endl;
+			exit(1);
+		}else{
+			return data[top--];
+		}
+	};
+	DataType getTop(){//获取栈顶
+		if(top==-1){
+			cout<<"栈已空!"<<endl;
+			exit(0);
+		}else{
+			return data[top];
+		}
+	};
+	void clear(){//清空
+		top=-1;
+	};
+};
+struct Node{//栈链式表的结构体
+	DataType data;//储存数据类型
+	Node *next;//结构体的指针
+};
+class Stack_2{//链式表实现栈的类
+	Node *top;
+public://公共方法
+	Stack_2(){//构造方法
+		top = new Node;
+		top->next = NULL;
+	};
+	void push(DataType d){
+		Node *n;
+		n = new Node;
+		n->data = d;
+		n->next = top->next;
+		top->next = n;
+	};
+	DataType pop(){
+		if(top->next!=NULL){
+			DataType d;
+			d = top->next->data;
+			top->next = top->next->next;
+			return d;
+		}else{
+			cout<<"栈为空!"<<endl;
+			exit(1);
+		}
+	};
+	bool ifEmpty(){
+		return top->next==NULL?1:0;
+	};
+	void clear(){
+		Node *n,*m;
+		n = top->next;
+		while(n!=NULL){
+			m = n;
+			n = n->next;
+			delete m;
+		}
+	};
+};
+int main(){
+	Stack_1 s1;
+	cout<<s1.ifEmpty()<<endl;
+	//
+	Stack_2 s2;
+	s2.push('z');
+	s2.push('x');
+	s2.push('c');
+	cout<<s2.pop()<<endl;
+	cout<<s2.pop()<<endl;
+	cout<<s2.pop()<<endl;
+	cout<<s2.pop()<<endl;
+	return 0;
+}
